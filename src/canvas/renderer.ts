6bb8e3819a92155900dfba74acceb56e0cea4d88
opdevlugt-tech <opdevlugt-tech@users.noreply.github.com>
2026-07-15 16:49:01 +0200

    fix: restore renderer imports after dim cleanup

--- src/canvas/renderer.ts
+++ src/canvas/renderer.ts
@@ -11,8 +11,10 @@ import {
   nearPoint,
   isCanonicalAngle,
   interiorBisectorRad,
+  interiorExterior,
   interiorExteriorAt,
   polygonWindingSign,
+  relativeTurnDeg,
   wallPiecesWithDoors,
   wallSegments,
 } from '../geometry/math';
@@ -784,21 +786,4 @@ function drawDimText(
   ctx.textAlign = 'center';
   ctx.textBaseline = 'middle';
   ctx.fillText(text, x, y);
-}
-
-function roundRect(
-  ctx: CanvasRenderingContext2D,
-  x: number,
-  y: number,
-  w: number,
-  h: number,
-  r: number,
-): void {
-  ctx.beginPath();
-  ctx.moveTo(x + r, y);
-  ctx.arcTo(x + w, y, x + w, y + h, r);
-  ctx.arcTo(x + w, y + h, x, y + h, r);
-  ctx.arcTo(x, y + h, x, y, r);
-  ctx.arcTo(x, y, x + w, y, r);
-  ctx.closePath();
 }
